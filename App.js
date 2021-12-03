ad88f5294eb7ca1069699d2371f8f58449e08efa
Cosmin Handaric <76441037+cosmin22h@users.noreply.github.com>
2021-12-03 22:27:20 +0200

    db connection

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const callTest = async () => {
+    const response = await fetch(
+      'https://pulihack-default-rtdb.europe-west1.firebasedatabase.app/user.json',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: 'user1',
+          description: 'plm',
+        }),
+      }
+    );
+  };
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Go" onPress={callTest} />
     </View>
   );
 }
