b208bb266c5ecd0bc2c81b951f388482bf9a5451
Alexandru Bivolaru <bivolaru.alexandru14@gmail.com>
2021-12-05 01:45:16 +0200

    logo added

--- App.js
+++ App.js
@@ -1,18 +1,13 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import Root from './components/Root';
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import Root from "./components/Root";
 
-import rootReducer from './store/reducer';
+import rootReducer from "./store/reducer";
 const store = createStore(rootReducer);
 
-import SpinnerThingy from './components/SpinnerThingy';
+import SpinnerThingy from "./components/SpinnerThingy";
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <Root />
-    </Provider>
-    // <SpinnerThingy />
-  );
+  return <SpinnerThingy />;
 }
