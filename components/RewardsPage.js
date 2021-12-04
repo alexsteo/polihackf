fe3ddb279c7abcd2e51151829b503772d7ea8859
Cosmin Handaric <76441037+cosmin22h@users.noreply.github.com>
2021-12-05 01:46:22 +0200

    habbit

--- components/RewardsPage.js
+++ components/RewardsPage.js
@@ -1,43 +1,80 @@
-import React, { useEffect, useState } from "react";
-import { CategoryCard, Image, FlatList, SafeAreaView, Text, StyleSheet, View, TextInput, Button } from "react-native";
+import React, { useEffect, useState } from 'react';
+import {
+  Image,
+  FlatList,
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  ImageBackground,
+  Dimensions,
+} from 'react-native';
 
-import { fetchRewards } from "../services/reward-services/RewardServiceHandler";
+import forest_image from '../images/forest2.png';
 
-const imageData = require("../assets/imageData.json");
+import { fetchRewards } from '../services/reward-services/RewardServiceHandler';
+
+var width = Dimensions.get('window').width; //full width
+var height = Dimensions.get('window').height; //full height
+
+const imageData = require('../assets/imageData.json');
 
 const Item = ({ reward }) => (
-    <View style={{marginTop: 50, flexDirection: 'row'}}>
-        <Image style={{flex: 1}} source={{uri: imageData[reward.img], width:180, height:180}}/>
-        <View style={{flex: 1}} >
-            <Text>{reward.name}</Text>
-            <Text>{reward.description}</Text>
-            <Text>{reward.price}</Text>
-        </View> 
-        <Button title='Buy' style={{flex: 1}} />
+  <View style={{ marginTop: 50, flexDirection: 'row' }}>
+    <Image
+      style={{ flex: 1 }}
+      source={{ uri: imageData[reward.img], width: 180, height: 180 }}
+    />
+    <View style={{ flex: 1 }}>
+      <Text>{reward.name}</Text>
+      <Text>{reward.description}</Text>
+      <Text>{reward.price}</Text>
     </View>
-    );
+    <TouchableOpacity style={styles.btnContainer}>
+      <Text>Buy</Text>
+    </TouchableOpacity>
+  </View>
+);
 const RewardsPage = () => {
-    const [rewards, setRewards] = useState([]);
-    const renderItem = ({ item }) => (
-        <Item reward={item} />
-      );
-    
-    useEffect(async () => {
-        setRewards(await fetchRewards());
-    }, []);
-    
-    return (
+  const [rewards, setRewards] = useState([]);
+  const renderItem = ({ item }) => <Item reward={item} />;
+
+  useEffect(async () => {
+    setRewards(await fetchRewards());
+  }, []);
+
+  return (
     <View>
-        <FlatList
+      {/* <ImageBackground
+        source={forest_image}
+        resizeMode="cover"
+        style={styles.imageStyle}
+        blurRadius={1}
+      > */}
+      <FlatList
         data={rewards}
         renderItem={renderItem}
         keyExtractor={item => item.key}
-        />
+      />
+      {/* </ImageBackground> */}
     </View>
   );
 };
 const styles = StyleSheet.create({
-  //Check project repo for styles
+  btnContainer: {
+    alignSelf: 'center',
+    padding: 10,
+    backgroundColor: '#EDEAC2',
+  },
+  imageStyle: {
+    paddingTop: '60%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: width,
+    height: height + 100,
+    flex: 1,
+    justifyContent: 'center',
+  },
 });
 
 export default RewardsPage;
