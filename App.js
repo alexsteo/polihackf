872020b43cd2459318476a2dc05eaf9fab1dcc05
Alexandru Bivolaru <bivolaru.alexandru14@gmail.com>
2021-12-04 21:59:31 +0200

    added swipe for cards

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import HomeNavigator from "./navigation/HomeNavigator";
 import { View, Text, StyleSheet } from "react-native";
+import ViewTipsList from "./components/ViewTips";
 
 export default function App() {
-  return <HomeNavigator />;
+  return <ViewTipsList />;
 }
 
 const styles = StyleSheet.create({
