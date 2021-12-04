51bda4ae0a08bdef90c4b7e70ffe238a7df0598c
Alexandru Bivolaru <bivolaru.alexandru14@gmail.com>
2021-12-04 14:37:33 +0200

    pushed the swipe component

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 import RegisterForm from "./components/RegisterForm";
+import SomeComponent from "./screens/swipeTest";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <RegisterForm />
+      <SomeComponent />
     </View>
   );
 }
