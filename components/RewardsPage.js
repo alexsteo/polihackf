6f5599a292d457e37c3a7a9e5233b8fa11ea33b6
snoobism <nitaeduarddd@gmail.com>
2021-12-05 02:18:49 +0200

    Added user score system integration

--- components/RewardsPage.js
+++ components/RewardsPage.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
+import {useSelector} from 'react-redux';
 import { CategoryCard, Image, FlatList, Text, StyleSheet, View, TextInput, Button } from "react-native";
 
 import { fetchRewards } from "../services/reward-services/RewardServiceHandler";
+import {updateUser} from "../services/user-services/UserServiceHandler";
 
 const imageData = require("../assets/imageData.json");
 
-const Item = ({ reward }) => (
+const Item = ({ reward, user, buyItem }) => (
     <View style={{marginTop: 50, flexDirection: 'row'}}>
         <Image style={{flex: 1, width:80, height:80, resizeMode: "contain"}} source={{uri: imageData[reward.img]}}/>
         <View style={{flex: 1}} >
@@ -13,13 +15,14 @@ const Item = ({ reward }) => (
             <Text>Description: {reward.description}</Text>
             <Text>Price: {reward.points}</Text>
         </View> 
-        <Button title='Buy' style={{flex: 1}} />
+        <Button title='Buy' style={{flex: 1}} onPress={() => buyItem(reward)}/>
     </View>
     );
 const RewardsPage = () => {
+    const user = useSelector(state => state.auth.user);
     const [rewards, setRewards] = useState([]);
     const renderItem = ({ item }) => (
-        <Item reward={item} />
+        <Item reward={item} user={user} buyItem={buyItem}/>
       );
     
     useEffect(async () => {
@@ -27,8 +30,15 @@ const RewardsPage = () => {
         console.log(rewards);
     }, []);
     
+    const buyItem = (reward) => {
+        user.score -= reward.points;
+        updateUser(user);
+        setRewards(rewards.filter(rew => rew.name !== reward.name));
+    }
+
     return (
     <View>
+        <Text style={{marginTop:50, fontSize:18,textAlign:"center"}}>Reward Points: {user.score}</Text>
         <FlatList
         data={rewards}
         renderItem={renderItem}
